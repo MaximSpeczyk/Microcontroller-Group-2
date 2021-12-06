d214a6c1d5a3e3456d73d9267b588198a37c67b6
ArSamir <92159244+ArSamir@users.noreply.github.com>
2021-12-06 11:32:12 +0100

    Update pedTL.cc

--- Arsany Samir Kamel/PedestrianTL/pedTL.cc
+++ Arsany Samir Kamel/PedestrianTL/pedTL.cc
@@ -4,11 +4,12 @@
 #define Yellow 10
 #define Green 9
 #define Request 2
+#define time 5000
 
-int path = 0;
+int path = 1;
 
 enum Color{redC, yellowC, greenC};
-Color current = yellowC;
+Color current = redC;
 
 void pedestreian()
 {
@@ -22,64 +23,74 @@ void setup()
   pinMode(Red, OUTPUT);
   pinMode(Yellow, OUTPUT);
   pinMode(Green, OUTPUT);
-  pinMode(Request, INPUT);
   attachInterrupt(digitalPinToInterrupt(Request), pedestreian, FALLING);
 }
 
 void redL()
 {
     digitalWrite(Red, HIGH);
-  	digitalWrite(RequestGreen, HIGH);
-    delay(5000);
-  	digitalWrite(RequestGreen, LOW);
+    delay(time);
     digitalWrite(Red, LOW);
-    current = redC;
+    current = yellowC;
 }
   
 void yellowL()
 {	
     digitalWrite(Yellow, HIGH);
-  	digitalWrite(RequestRed, HIGH);
-    delay(2500);
- 	digitalWrite(RequestRed, LOW);
+    delay(time/2);
     digitalWrite(Yellow, LOW);
-    current = yellowC;
 }
   
 void greenL()
 {
   	digitalWrite(Green, HIGH);
-  	digitalWrite(RequestRed, HIGH);
-    delay(5000);
-  	digitalWrite(RequestRed, LOW);
+    delay(time);
     digitalWrite(Green, LOW);
-    current = greenC;
+    current = yellowC;
 }
 
+void pdRedL(){
+  
+  digitalWrite(RequestGreen, LOW);
+  digitalWrite(RequestRed, HIGH);
+}
+
+void pdGreenL(){
+  
+  digitalWrite(RequestGreen, HIGH);
+  digitalWrite(RequestRed, LOW);
+
+}
 
 void loop()
 {
   switch(current)
   {
     case redC:
-      yellowL();
+    	pdGreenL();
+      	redL();
+      
     break;
       
     case yellowC:
+    	pdRedL();
+    	yellowL();
+    	
       	if(path == 1)
         {
           	path = 0;
-      		greenL();
+      		current = greenC;
         }
       	else if(path == 0)
         {	
             path = 1;
-      		redL();
+      		current = redC;
         }
     break;
       
     case greenC:
-        yellowL();
+        greenL();
+    	pdRedL();
     break;
   }
 } 
